7b89f06458d175afff4a4f470ba596bb8ddf1263
Robert Horlings <robert@isdat.nl>
2018-10-12 16:13:53 +0200

    Fix travis deploy script

--- ci/helm/deploy.sh
+++ ci/helm/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ENVIRONMENT=${1:-ci}
+
 helm repo add fairspace https://fairspace.github.io/helm-repo
 helm repo update
-helm upgrade --install hyperspace-ci fairspace/hyperspace --namespace=hyperspace-ci -f ./ci/ci-values.yaml
+helm upgrade --install hyperspace-${ENVIRONMENT} fairspace/hyperspace --namespace=hyperspace-${ENVIRONMENT} -f ./ci/${ENVIRONMENT}-values.yaml
