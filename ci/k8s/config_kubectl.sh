1c09ef89777a6ea878dfdc6cee9be80a77df81e3
Robert Horlings <robert@isdat.nl>
2018-10-15 09:32:58 +0200

    Update kubectl configuration and small travis fixes

--- ci/k8s/config_kubectl.sh
+++ ci/k8s/config_kubectl.sh
@@ -3,4 +3,4 @@
 # The specific kube-config file is stored encrypted
 #
 mkdir -p $HOME/.kube
-openssl aes-256-cbc -K $encrypted_4e2f7b28d92d_key -iv $encrypted_4e2f7b28d92d_iv -in ci/kube-config.yml.enc -out $HOME/.kube/config -d
+openssl aes-256-cbc -K $encrypted_985abdf32880_key -iv $encrypted_985abdf32880_iv -in ci/kube-config.yml.enc -out $HOME/.kube/config -d
