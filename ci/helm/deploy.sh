123fa487666bf6c1788c61b0b58870cf79714d5d
Robert Horlings <robert@isdat.nl>
2018-10-17 14:23:28 +0200

    Use external build scripts to DRY

--- ci/helm/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ENVIRONMENT=${1:-ci}
-
-helm upgrade --install hyperspace-${ENVIRONMENT} fairspace/hyperspace --namespace=hyperspace-${ENVIRONMENT} --version $VERSION -f ./ci/config/${ENVIRONMENT}-values.yaml
--- ci/k8s/config_kubectl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# The specific kube-config file is stored encrypted
-#
-mkdir -p $HOME/.kube
-openssl aes-256-cbc -K $encrypted_985abdf32880_key -iv $encrypted_985abdf32880_iv -in ci/kube-config.yml.enc -out $HOME/.kube/config -d
