37b3e70782ebb3d116453416e5a64c83158fba34
Pavel Mikhailovskii <pavel@thehyve.nl>
2019-08-16 17:17:15 +0200

    Update projects/portal/.travis/build.sh
    Co-Authored-By: Robert Horlings <robert@isdat.nl>

--- projects/portal/.travis/build.sh
+++ projects/portal/.travis/build.sh
@@ -5,7 +5,7 @@ $BUILD_SCRIPTS_DIR/gradle/tag.sh
 ./gradlew clean build test jacocoTestReport $GRADLE_OPTIONS
 
 # Show the code coverage report on the command line
-# First remove the 'io.fairspace.saturn' package prefix to simplify output
+# First remove the 'io.fairspace.portal' package prefix to simplify output
 # After that, only select columns 2 - 7 (disregarding app name and line,
 # complexity and method coverage)
 # Finally, show the output as a table
