7cd249322e972303d1143e2f0dd7ed6551ba255f
Robert Horlings <robert@isdat.nl>
2019-08-19 12:07:36 +0200

    Change color scheme for frontend

--- projects/frontend/src/app/App.theme.js
+++ projects/frontend/src/app/App.theme.js
@@ -1,13 +1,13 @@
 import {createMuiTheme} from "@material-ui/core";
-import {green, lightGreen, red} from "@material-ui/core/colors";
+import {grey, blue, blueGrey} from "@material-ui/core/colors";
 
 export default createMuiTheme({
     typography: {
         useNextVariants: true,
     },
     palette: {
-        primary: process.env.NODE_ENV === 'development' ? lightGreen : green,
-        secondary: red
+        primary: process.env.NODE_ENV === 'development' ? { main: blueGrey.A200 } : blueGrey,
+        secondary: blue
     },
     props: {
         MuiPaper: {
