31ac09c969f9ec182edce2118b7451fafb3faf04
Robert Horlings <robert@isdat.nl>
2019-08-19 10:11:07 +0200

    Update layout colors for portal frontend

--- projects/frontend/src/app/App.theme.js
+++ projects/frontend/src/app/App.theme.js
@@ -1,15 +1,13 @@
 import {createMuiTheme} from "@material-ui/core";
-import indigo from "@material-ui/core/colors/indigo";
-import pink from "@material-ui/core/colors/pink";
-import blue from '@material-ui/core/colors/blue';
+import {green, lightGreen, red} from "@material-ui/core/colors";
 
 export default createMuiTheme({
     typography: {
         useNextVariants: true,
     },
     palette: {
-        primary: process.env.NODE_ENV === 'development' ? blue : indigo,
-        secondary: pink
+        primary: process.env.NODE_ENV === 'development' ? lightGreen : green,
+        secondary: red
     },
     props: {
         MuiPaper: {
