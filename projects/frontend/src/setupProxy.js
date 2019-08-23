2c7116af21ecc5c4291fed681e3f7f8da85430af
strangepleasures <pavel@thehyve.nl>
2019-08-23 13:05:57 +0200

    Don't use the back-end for local development of the front-end

--- projects/frontend/src/setupProxy.js
+++ projects/frontend/src/setupProxy.js
@@ -4,6 +4,4 @@ module.exports = (app) => {
     app.use(proxy('/config', {target: 'http://localhost:5000/'}));
     app.use(proxy('/api/v1/account', {target: 'http://localhost:5000/'}));
     app.use(proxy('/api/v1/workspaces', {target: 'http://localhost:5000/'}));
-
-    app.use(proxy('/api/v1', {target: 'http://localhost:8080/'}));
 };
