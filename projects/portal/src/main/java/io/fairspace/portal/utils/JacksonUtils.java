1cd0f7679589829e15e173b730747e85d0af02e4
strangepleasures <pavel@thehyve.nl>
2019-08-30 17:39:51 +0200

    Add JavaDoc

--- projects/portal/src/main/java/io/fairspace/portal/utils/JacksonUtils.java
+++ projects/portal/src/main/java/io/fairspace/portal/utils/JacksonUtils.java
@@ -3,6 +3,17 @@ package io.fairspace.portal.utils;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class JacksonUtils {
+    /**
+     * Merges two ObjectNode objects, applying changes to the left object.
+     * Adds to the left object all values from right which are missing in the left.
+     * Replaces all values which present on both sides and at least one value is not an object with values from the right.
+     * If values on both sides are objects, recursively merges them.
+     * No special logic for arrays.
+     *
+     * @param left
+     * @param right
+     * @return the left object
+     */
     public static ObjectNode merge(ObjectNode left, ObjectNode right) {
         for (var it = right.fields(); it.hasNext(); ) {
             var entry = it.next();
