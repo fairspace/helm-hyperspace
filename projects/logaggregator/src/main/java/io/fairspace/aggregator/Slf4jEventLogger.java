3820d0ed48406aa4be1ebf3787b53d21dc605120
strangepleasures <pavel@thehyve.nl>
2019-09-25 12:14:00 +0200

    Remove debug leftovers

--- projects/logaggregator/src/main/java/io/fairspace/aggregator/Slf4jEventLogger.java
+++ projects/logaggregator/src/main/java/io/fairspace/aggregator/Slf4jEventLogger.java
@@ -1,7 +1,6 @@
 package io.fairspace.aggregator;
 
 import io.fairspace.aggregator.model.EventContainer;
-import org.apache.log4j.PropertyConfigurator;
 
 import java.util.Date;
 
@@ -9,14 +8,9 @@ import static java.lang.String.format;
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class Slf4jEventLogger implements EventLogger {
-//    Slf4jEventLogger() {
-//        PropertyConfigurator.configure("log4j.properties");
-//    }
-
     @Override
     public void log(Date timestamp, EventContainer eventContainer, String payload) {
         var log = getLogger(format("audit.%s.%s.%s", eventContainer.getEvent().getCategory(), eventContainer.getEvent().getEventType(), eventContainer.getWorkspace()));
-        getLogger(getClass()).error("ERRRRRROR");
         log.info("timestamp: {}, workspace: {}, user id: {}, user name: {}, payload: {}",
                 timestamp,
                 eventContainer.getWorkspace(),
