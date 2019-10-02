c27585a0b8fd9d059e5a4deb9b5c18e6f8671907
strangepleasures <pavel@thehyve.nl>
2019-10-02 14:10:37 +0200

    Optimize imports

--- projects/portal/src/main/java/io/fairspace/portal/utils/JacksonUtils.java
+++ projects/portal/src/main/java/io/fairspace/portal/utils/JacksonUtils.java
@@ -3,7 +3,6 @@ package io.fairspace.portal.utils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
