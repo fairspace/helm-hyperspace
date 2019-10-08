101166e58e56fc0ebb63244cc62de347e2d5084a
strangepleasures <pavel@thehyve.nl>
2019-10-08 16:06:45 +0200

    Simplify implementation of useRepeat

--- projects/frontend/src/common/hooks/UseRepeat.js
+++ projects/frontend/src/common/hooks/UseRepeat.js
@@ -1,4 +1,4 @@
-import {useEffect, useRef} from "react";
+import {useEffect} from "react";
 
 /**
  * Performs a promise-returning action periodically with a fixed delay between executions.
@@ -7,18 +7,13 @@ import {useEffect, useRef} from "react";
  * @param delay
  */
 export default (callback, delay) => {
-    const id = useRef();
-
     // Set up the interval.
     useEffect(() => {
-        function tick() {
-            callback()
-                .finally(() => {
-                    id.current = setTimeout(tick, delay);
-                });
-        }
-
-        id.current = setTimeout(tick, delay);
-        return () => clearTimeout(id.current);
+        let id = setTimeout(function tick() {
+            callback().finally(() => {
+                id = setTimeout(tick, delay);
+            });
+        }, delay);
+        return () => clearTimeout(id);
     }, [callback, delay]);
 };
