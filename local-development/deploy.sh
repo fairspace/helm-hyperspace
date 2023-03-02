13e0aa2a17c673f493c7554e5a71df6c46a85c2b
FemkeK <femke@kopmels.com>
2023-03-02 13:34:19 +0100

    Remove incorrect comment

--- local-development/deploy.sh
+++ local-development/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 here=$(realpath $(dirname "${0}"))
-helm_cmd=$(realpath ~/bin/helm3/helm)
+helm_cmd=$(realpath /opt/homebrew/bin/helm)
 
 # Prerequisites:
 # $ minikube start
@@ -13,7 +13,5 @@ pushd "${here}"
 ((${helm_cmd} repo list | cut -f1 | grep '^codecentric') || ${helm_cmd} repo add codecentric https://codecentric.github.io/helm-charts) && \
 ${helm_cmd} dependency update ../charts/fairspace-keycloak && \
 ${helm_cmd} package ../charts/fairspace-keycloak && \
-${helm_cmd} upgrade keycloak-local --install --namespace keycloak-dev fairspace-keycloak-0.0.0-RELEASEVERSION.tgz \
-  -f local-values.yaml
-
+${helm_cmd} upgrade keycloak-local --install --namespace keycloak-dev fairspace-keycloak-0.0.0-RELEASEVERSION.tgz -f local-values.yaml 
 popd
